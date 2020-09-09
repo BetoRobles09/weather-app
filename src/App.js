052a5c3788c1fbf2d953c3d25cbc03088a2d555b
Alberto Robles <beto.robles25@gmail.com>
2020-09-09 15:48:45 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Form from './components/Form'
+import Weather from './components/Weather'
+import Error from './components/Error'
 
-function App() {
+/*
+ global fetch
+*/
+
+function App () {
+  const [search, setSearch] = useState({
+    city: '',
+    country: ''
+  })
+  const [request, setRequest] = useState(false)
+  const [data, setData] = useState({})
+  const [error, setError] = useState(false)
+
+  const { city, country } = search
+
+  useEffect(() => {
+    const requestAPI = async () => {
+      if (request) {
+        const apiKey = 'efce7534c35c46b5367f2a95ff12ab54'
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`
+        const response = await fetch(url)
+        const result = await response.json()
+        setData(result)
+        setRequest(false)
+
+        if (result.cod === '404') {
+          setError(true)
+        } else {
+          setError(false)
+        }
+      }
+    }
+    requestAPI()
+  }, [request])
+
+  let component
+  if (error) {
+    component = <Error message='No results' />
+  } else {
+    component = <Weather data={data} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header title='Weather App' />
+      <div className='contenedor-form'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col m6 s12'>
+              <Form search={search} setSearch={setSearch} setRequest={setRequest} />
+            </div>
+            <div className='col m6 s12'>
+              {component}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Weather.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Weather = ({ data }) => {
+  const { name, main, weather } = data
+
+  if (!name) return null
+
+  const kelvin = 273.15
+
+  return (
+    <div className='card-panel whithe s12'>
+      <div className='black-text'>
+        <h2>The weather of {name} is: </h2>
+        <p className='temperatura'>{parseFloat(main.temp - kelvin, 10).toFixed(2)} <span>&#x2103;</span> </p>
+        <p>Feels like: {parseFloat(main.feels_like - kelvin, 10).toFixed(2)}</p>
+        <p>Weather: {weather[0].main}</p>
+        <p>Max Temp: {parseFloat(main.temp_max - kelvin, 10).toFixed(2)} <span>&#x2103;</span> </p>
+        <p>Min Temp: {parseFloat(main.temp_min - kelvin, 10).toFixed(2)} <span>&#x2103;</span> </p>
+      </div>
+    </div>
+  )
+}
+
+export default Weather
